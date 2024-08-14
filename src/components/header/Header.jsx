7d786d8998672cb1fff4b228bdda1e75944461ee
tech-kinako <tech.kinako@gmail.com>
2024-08-14 23:56:34 +0900

    [PG] staion3「スレッド作成画面を作ろう」を実装

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
 import classes from "./header.module.scss";
 
-export const Header = () => {
+export const Header = (props) => {
+  const dspCreateThreadLink = ()=> {
+    if (props.isLink === "true") {
+      return <Link to="threads/new" className={classes.threadLink} >スレッドをたてる</Link>
+    }
+  }
+
   return (
     <header>
       <h1 className={classes.headerTitle}>掲示板</h1>
-      <a className={classes.threadLink} href="">スレッドをたてる</a>
+      {dspCreateThreadLink()}
     </header>
   );
 };
